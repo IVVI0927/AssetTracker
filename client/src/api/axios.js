ea62778c859878aba0ebc89e074722f3c6510cc9
IVVI0927 <2582908202@qq.com>
2025-07-14 23:31:15 +0800

    AWS backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5050', // 统一配置后端地址
+  //baseURL: 'http://localhost:5050', // 统一配置后端地址
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
